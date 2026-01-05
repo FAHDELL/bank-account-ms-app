6c27d2ea6e5002a22e5869a90e6b4241aaf8f299
fahdellahia <ellahiafahd@gmail.com>
2026-01-05 14:06:49 +0100

    Message décrivant la mise à jour

--- customer-service/src/main/java/fahd/net/customer_service/repositories/CustomerRepository.java
+++ customer-service/src/main/java/fahd/net/customer_service/repositories/CustomerRepository.java
@@ -2,8 +2,7 @@ package fahd.net.customer_service.repositories;
 
 import fahd.net.customer_service.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+
 public interface CustomerRepository  extends JpaRepository<Customer,Long> {
 }
